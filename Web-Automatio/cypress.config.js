4a68146cac8243232c9a5a203ec0a4ebf5f0d9ce
hina-tester <hinasarmad@gmail.com>
2025-11-28 11:19:08 +0500

    Updated confir file,read me file and report folder

--- Web-Automatio/cypress.config.js
+++ Web-Automatio/cypress.config.js
@@ -1,47 +1,34 @@
 const { defineConfig } = require("cypress");
 
-const { defineConfig } = require("cypress");
-const terminalReport = require('cypress-terminal-report/src/installLogs');
-
-
 module.exports = defineConfig({
 
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   video: true,
-  
-  reporterOptions: {
-    overwrite: false, 
-    html: true,
-    json: true, // Generate JSON report
-    reportFilename: '[name]',  // Use ONLY [timestamp]
-    reportPageTitle: 'Test Report',
-    reportDir: 'cypress/custom-reports',
-    embeddedScreenshots: true,
-    inlineAssets: true,
 
-    
+  reporterOptions: {
+    overwrite: false,
+    html: true,
+    json: true,                 // Generate JSON report
+    reportDir: "cypress/reports",
+    reportFilename:"[name]" +`report-${new Date().toISOString().replace(/[:.]/g, '-')}`,
+    reportPageTitle: "Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true
   },
-    projectId: "2hxc8g",
+
+  projectId: "2hxc8g",
 
   e2e: {
+    chromeWebSecurity: false,
+    experimentalStudio: true,
 
-     chromeWebSecurity: false,
-  
-    experimentalStudio:true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-       require('cypress-mochawesome-reporter/plugin')(on);
-       terminalReport(on, config);
-       return config;
-   
-    },
-       
-    },
-  
-
-  env:{
-
-    URL:'https://flacronbuild.com/'
-
+      // Register Mochawesome plugin
+      require("cypress-mochawesome-reporter/plugin")(on);
+    }
   },
+
+  env: {
+    URL: "https://flacronbuild.com/"
+  }
 });
